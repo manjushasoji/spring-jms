7297353ce58d9bbe7451324bc172574b28cfc675
manjushasoji <manjushajoy@hotmail.com>
2021-01-07 18:47:07 -0600

    added send and receive class files

--- src/main/java/manj/springframework/springjms/listener/HelloMessageListener.java
+++ src/main/java/manj/springframework/springjms/listener/HelloMessageListener.java
@@ -1,31 +1,49 @@
 package manj.springframework.springjms.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
 import manj.springframework.springjms.config.JmsConfig;
 import manj.springframework.springjms.model.HelloWorldMessage;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
-	
+
+	private final JmsTemplate jmsTemplate;
+
 	@JmsListener(destination = JmsConfig.MY_QUEUE)
-	public void listen(@Payload HelloWorldMessage helloWorldMessage,
-						@Headers MessageHeaders messageHeaders, Message message) {
+	public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders messageHeaders,
+			Message message) {
+
+		//System.out.println("I Got a Message!!!!!");
+
+		//System.out.println(helloWorldMessage);
+
+		// uncomment and view to see retry count in debugger
+		// throw new RuntimeException("foo");
+
+	}
+
+	@JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+	public void listenForHello(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers,
+			Message message) throws JMSException {
+
+		HelloWorldMessage payloadMsg = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Reply Message").build();
+
+		jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 		
-		System.out.println("I Got a Message!!!!!");
-
-        System.out.println(helloWorldMessage);
-
-
-        // uncomment and view to see retry count in debugger
-       // throw new RuntimeException("foo");
-		
+		System.out.println("Sending Reply Message");
 	}
 
 }
--- src/main/java/manj/springframework/springjms/sender/HelloSender.java
+++ src/main/java/manj/springframework/springjms/sender/HelloSender.java
@@ -2,9 +2,17 @@ package manj.springframework.springjms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import manj.springframework.springjms.config.JmsConfig;
@@ -14,17 +22,46 @@ import manj.springframework.springjms.model.HelloWorldMessage;
 @RequiredArgsConstructor
 public class HelloSender {
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper objectMapper;
 
 	@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
 
-		System.out.println("I'm Sending a message");
+		//System.out.println("I'm Sending a message");
 
 		HelloWorldMessage message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello World!").build();
 
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-		System.out.println("Message Sent!");
+		//System.out.println("Message Sent!");
 
+	}
+
+	@Scheduled(fixedRate = 2000)
+	public void sendandReceiveMessage() throws JMSException {
+
+		HelloWorldMessage message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello").build();
+
+		Message receviedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				Message helloMessage = null;
+
+				try {
+					helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+					helloMessage.setStringProperty("_type", "manj.springframework.springjms.model.HelloWorldMessage");
+
+					System.out.println("Sending Hello");
+
+					return helloMessage;
+
+				} catch (JsonProcessingException e) {
+					throw new JMSException("----JMS Exception !!!");
+				}
+			}
+		});
+
+		System.out.println(receviedMsg.getBody(String.class));
 	}
 }
